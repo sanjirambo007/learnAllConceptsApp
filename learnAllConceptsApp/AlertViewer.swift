3aa9e60ccd07056fd94cc2916309dcc39a65bab2
Sanjit Y <sanjity@Sanjits-Mac.local>
2019-02-13 10:31:02 -0800

    First commit.

---
+++ learnAllConceptsApp/AlertViewer.swift
@@ -0,0 +1,11 @@
+//
+//  AlertViewer.swift
+//  learnAllConceptsApp
+//
+//  Created by Sanjit Y on 2/13/19.
+//  Copyright © 2019 Sanjit Y. All rights reserved.
+//
+
+import Foundation
+
+
--- learnAllConceptsApp/ViewController.swift
+++ learnAllConceptsApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterCityName: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func fetchButtonTapped(_ sender: Any) {
+        
 
+        if enterCityName.text?.isEmpty == true {
+            let alert = UIAlertController(title: "Empty Field", message: "'Place Name' text field is blank!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+    
+}
+    
 }
 
